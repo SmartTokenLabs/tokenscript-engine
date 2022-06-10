76495355418b41bde62a7bedaa3c7b9f64d28987
Michael Wallace <micwallace@gmx.com>
2022-06-10 22:48:46 +1000

    Update DefaultKeyValueStorage for iOS CI
    Not actually implemented yet haha.

--- src/iosMain/kotlin/org/tokenscript/engine/storage/DefaultKeyValueStorage.kt
+++ src/iosMain/kotlin/org/tokenscript/engine/storage/DefaultKeyValueStorage.kt
@@ -1,13 +1,15 @@
 package org.tokenscript.engine.storage
 
-actual open class DefaultKeyValueStorage {
+actual open class DefaultKeyValueStorage actual constructor(basePath: String, extension: String) {
 
-    actual fun readValue(path: String): String {
+    actual fun readValue(path: String): String? {
         TODO("Not yet implemented")
+        return NSString.stringWithContentsOfFile(path, NSUTF8StringEncoding, null) ?: return null
     }
 
     actual fun writeValue(path: String, value: String){
         TODO("Not yet implemented")
+        return (value as NSString).writeToFile(path, true, NSUTF8StringEncoding, null)
     }
 
     actual fun removeValue(path: String){
