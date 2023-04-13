5ad251a17ed32d7482140101a210bb8cee5d3706
Michael Wallace <micwallace@gmx.com>
2023-04-13 17:19:17 +1000

    chore: add mock transaction process

--- javascript/tokenscript-viewer/src/components/viewers/integration/steps/view-step.tsx
+++ javascript/tokenscript-viewer/src/components/viewers/integration/steps/view-step.tsx
@@ -1,6 +1,6 @@
-import {Component, Prop, h, State, Element} from "@stencil/core";
+import {Component, Prop, h, State, Element, JSX} from "@stencil/core";
 import {IntegrationViewer} from "../integration-viewer";
-import {TokenScript} from "@tokenscript/engine-js/src/TokenScript";
+import {ITransactionStatus, TokenScript} from "@tokenscript/engine-js/src/TokenScript";
 import {AbstractViewBinding} from "../../../../integration/abstractViewBinding";
 import {Card} from "@tokenscript/engine-js/src/tokenScript/Card";
 import {ViewEvent} from "@tokenscript/engine-js/src/view/ViewController";
@@ -28,26 +28,42 @@ export class ViewStep {
 	viewBinding;
 
 	async componentDidLoad() {
-		this.viewBinding = new ViewBinding(this.host);
+		this.viewBinding = new ViewBinding(this);
 		this.viewBinding.setTokenScript(this.tokenScript);
 		this.tokenScript.setViewBinding(this.viewBinding);
 		this.tokenScript.getViewController().showCard(this.card);
 	}
 
+	showToast(type: 'success'|'info'|'warning'|'error', title: string, description: string|JSX.Element){
+
+		const cbToast = this.host.querySelector(".toast") as HTMLCbToastElement;
+
+		cbToast.Toast({
+			title,
+			description,
+			timeOut: 30000,
+			position: 'top-right',
+			type
+		});
+	}
+
 	render() {
 		return (
-			<div class="card-container">
-				<div style={{position: "relative"}}>
-					<div class="view-loader" style={{display: "none"}}>
-						<loading-spinner/>
+			<div>
+				<div class="card-container">
+					<div style={{position: "relative"}}>
+						<div class="view-loader" style={{display: "none"}}>
+							<loading-spinner/>
+						</div>
+						<iframe class="tokenscript-frame"
+								sandbox="allow-scripts allow-modals allow-forms">
+						</iframe>
 					</div>
-					<iframe class="tokenscript-frame"
-							sandbox="allow-scripts allow-modals allow-forms">
-					</iframe>
+					<div class="action-bar" style={{display: "none"}}>
+						<button class="action-btn btn btn-primary"></button>
+					</div>
 				</div>
-				<div class="action-bar" style={{display: "none"}}>
-					<button class="action-btn btn btn-primary"></button>
-				</div>
+				<cb-toast className="toast"></cb-toast>
 			</div>
 		);
 	}
@@ -55,17 +71,57 @@ export class ViewStep {
 
 class ViewBinding extends AbstractViewBinding {
 
-	constructor(view: HTMLElement) {
-		super(view);
-
+	constructor(private viewStep: ViewStep) {
+		super(viewStep.host);
 	}
 
 	async confirmAction() {
+
 		const transaction = this.currentCard.getTransaction();
 
 		this.showLoader();
 
-		if (transaction){
+		const data: ITransactionStatus = {
+			status: "confirmed",
+			txNumber: "0x000",
+			txLink: "https://google.com/"
+		}
+
+		setTimeout(() => {
+
+			this.viewStep.showToast(
+				'info',
+				"Transaction submitted",
+				(<span>
+									{"Processing TX, please wait.. "}<br/>
+					{"TX Number: " + data.txNumber }
+								</span>)
+			);
+
+			setTimeout(() => {
+
+				this.viewStep.showToast(
+					'success',
+					"Transaction confirmed",
+					(<span>
+									{"TX " + data.txNumber + " confirmed!"}<br/>{
+						data.txLink ? <a href={data.txLink} target="_blank">{"View On Block Scanner"}</a> : ''}
+								</span>)
+				);
+
+				this.hideLoader();
+
+				setTimeout(() => {
+					console.log("Transaction completed!");
+
+					this.viewStep.viewer.returnResultToRequester(data);
+				}, 3000);
+
+			}, 5000);
+
+		}, 1000);
+
+		/*if (transaction){
 
 			console.log(transaction.getTransactionInfo());
 
@@ -73,24 +129,10 @@ class ViewBinding extends AbstractViewBinding {
 				await this.currentCard.executeTransaction((data) => {
 					switch (data.status){
 						case "submitted":
-							/*this.showToast(
-								'info',
-								"Transaction submitted",
-								(<span>
-									{"Processing TX, please wait.. "}<br/>
-									{"TX Number: " + data.txNumber }
-								</span>)
-							);*/
+
 							break;
 						case "confirmed":
-							/*this.showToast(
-								'success',
-								"Transaction confirmed",
-								(<span>
-									{"TX " + data.txNumber + " confirmed!"}<br/>{
-									data.txLink ? <a href={data.txLink} target="_blank">{"View On Block Scanner"}</a> : ''}
-								</span>)
-							);*/
+
 							break;
 					}
 				});
@@ -102,7 +144,9 @@ class ViewBinding extends AbstractViewBinding {
 		} else {
 			// this.iframe.contentWindow.onConfirm();
 			this.postMessageToView(ViewEvent.ON_CONFIRM, {});
-		}
+		}*/
+
+
 
 		this.hideLoader();
 	}
