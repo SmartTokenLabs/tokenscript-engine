70575087068f09660e266f63e6b07f204cc113d4
Justin Feng <realfengjia@foxmail.com>
2023-12-19 15:43:33 +0800

    Update token-viewer interface to handle sln attestation

--- javascript/tokenscript-viewer/src/components/viewers/joyid-token/token-viewer.tsx
+++ javascript/tokenscript-viewer/src/components/viewers/joyid-token/token-viewer.tsx
@@ -101,6 +101,26 @@ export class TokenViewer {
 			return true;
 		}
 
+		if (query.get("sln") === "true" && query.has("issuer") && query.has("uid")){
+			this.app.showTsLoader();
+
+			//TODO: load attestation from SLN-A
+			const attestation =
+				"eNrFU0uOWzEMu0vWQaG_rGUzmV6i6MKWrAMULdDj18kV3mIEwwvDpCSC_HmDb2S3OyKqnut-g38fZB79eLKXCo_xsSX4Bz4frkzwJLaNA3XfXp9NtvkMyzF8e8ps0C4sbZqxKpVwjK00snBMkBG1Vu2QSBTTepPgqExWYCWyFtdCYHIFclvaEXtgdPOEUc1wYORCTi172-YzCfmLZ7vh57I9KJ6fErXnA2AggTMnfrJ-z3Z_Pt5NQ3pOI4yzVQzF1dFDlHRO75ilZq62rSrnZJGNonMvEqQcKf0m8bZeEGuvs6YVLCLe0agkFIYYo4oNgHBRRrvOHB4J7cNy9nv9o74DhR-Q3-H98Of33_2ih0s1rsEhL-KBLqHxanvAl_RfWMrIxMxyvP0FJXhOZmCOEwldy2mdgXrJ5lUA3GC9JV-G112MuDnp5GUe10fa4NCTsTYq0xPZsmN_rNCxpSQbzQ9R1tF5UbHPuTRhNIWsxK0y86KFbnf49R_KlwhT";
+
+			console.log("Attestation loaded!");
+
+			this.app.hideTsLoader();
+
+			const params = new URLSearchParams();
+			params.set("attestation", attestation);
+			params.set("type", "eas");
+
+			this.loadAttestationAndTokenScript(params);
+
+			return true;
+		}
+
 		throw new Error("Could not locate token details using the values provided in the URL");
 	}
 
@@ -142,6 +162,18 @@ export class TokenViewer {
 			console.warn(e.message);
 		}
 	}
+
+  private async loadAttestationAndTokenScript(params: URLSearchParams) {
+    const { definition, tokenScript } = await this.app.tsEngine.importAttestationUsingTokenScript(params);
+
+    this.showToast.emit({
+      type: 'success',
+      title: 'Attestation imported',
+      description: 'Successfully imported ' + definition.meta.name,
+    });
+
+    this.tokenScript = tokenScript;
+  }
 
 	render(){
 
