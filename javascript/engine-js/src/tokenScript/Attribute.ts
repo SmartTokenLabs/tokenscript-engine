a9e8df40942efda5300ff12711a872a5ea8136e9
Michael Wallace <micwallace@gmx.com>
2023-12-21 17:35:11 +1100

    feat: Initial port of AW iOS filter logic

--- javascript/engine-js/src/tokenScript/Attribute.ts
+++ javascript/engine-js/src/tokenScript/Attribute.ts
@@ -39,6 +39,12 @@ export class Attribute {
 		return this.attributeDef.getAttribute("name");
 	}
 
+	public getAsType(){
+		const origins = this.getOrigins();
+		const origin = origins[0];
+		return origin.getAttribute("as");
+	}
+
 	/**
 	 * The label for the attribute
 	 */
