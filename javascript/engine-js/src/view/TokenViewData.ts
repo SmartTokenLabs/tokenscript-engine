dd2522b58a977903ed3239191ffbf559d38c1877
Michael Wallace <micwallace@gmx.com>
2024-01-16 12:11:25 +1100

    chore: remove redundant card Javascript

--- javascript/engine-js/src/view/TokenViewData.ts
+++ javascript/engine-js/src/view/TokenViewData.ts
@@ -69,44 +69,12 @@ export class TokenViewData {
 		const rpcURL = "${walletAdapter.getRpcUrl(tokenData.chainId)}";
 		const chainID = "${tokenData.chainId}";
 
-		/*window.web3CallBacks = {}
-
-		function executeCallback (id, error, value) {
-			console.debug('Execute callback: ' + id + ' ' + value)
-			window.web3CallBacks[id](error, value)
-			delete window.web3CallBacks[id]
-		}
-
-		web3 = {
-			personal: {
-				sign: function (msgParams, cb) {
-					const { data } = msgParams
-					const { id = 8888 } = msgParams
-					window.web3CallBacks[id] = cb
-					alpha.signPersonalMessage(id, data);
-				}
-			},
-			tokens: {
-				data: {
-					currentInstance: {},
-				},
-				dataChanged: (prevTokens, newTokens, id) => {
-					console.log('web3.tokens.data changed.')
-				}
-			},
-			action: {
-				setProps: function (msgParams) {
-					alpha.setValues(JSON.stringify(msgParams));
-				}
-			}
-		}
-
-		web3.tokens.data.currentInstance = _currentTokenInstance*/
-
+		// Injected card SDK
 		${CARD_SDK_V1}
 
 		window.tokenscript.setInstanceData(_currentTokenInstance);
 
+		// Extra initialisation
 		function refresh() {
 		   web3.tokens.dataChanged('test', web3.tokens.data, '${this.getViewDataId()}') //TODO: Cache previous value of token to feed into first arg
 		}
