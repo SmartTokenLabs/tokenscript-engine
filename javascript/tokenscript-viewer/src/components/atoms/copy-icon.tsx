8fc6e055b9a2396c17ca09665ff793b7d0d051d8
Nick <nicktaras@hotmail.co.uk>
2024-05-30 14:50:50 +1000

    Update copy-icon.tsx

--- javascript/tokenscript-viewer/src/components/atoms/copy-icon.tsx
+++ javascript/tokenscript-viewer/src/components/atoms/copy-icon.tsx
@@ -1,32 +1,32 @@
 import { Component, h, Host, Prop, State } from '@stencil/core';
 
 @Component({
-  tag: 'copy-icon',
-  styleUrl: 'copy-icon.css',
-  shadow: false,
-  scoped: false,
+	tag: 'copy-icon',
+	styleUrl: 'copy-icon.css',
+	shadow: false,
+	scoped: false,
 })
 export class CopyIcon {
-  @Prop()
-  copyText: string;
+	@Prop()
+	copyText: string;
 
-  @Prop()
-  height: string = '18px';
+	@Prop()
+	height: string = '18px';
 
-  @State()
-  clipboardAvailable: boolean = !!navigator.clipboard;
+	@State()
+	clipboardAvailable: boolean = !!navigator.clipboard;
 
-  copy() {
-    if (this.clipboardAvailable) {
-      navigator.clipboard.writeText(this.copyText);
-    }
-  }
+	copy() {
+		if (this.clipboardAvailable) {
+			navigator.clipboard.writeText(this.copyText);
+		}
+	}
 
-  render() {
-    return (
-      <Host style={{ height: this.height }}>
-        {this.clipboardAvailable && <img class="copy-icon" alt="copy" title="Copy" src="/assets/icon/copy.svg" onClick={() => this.copy()} />}
-      </Host>
-    );
-  }
+	render() {
+		return (
+			<Host style={{ height: this.height }}>
+				{this.clipboardAvailable && <img class="copy-icon" alt="copy" title="Copy" src="/assets/icon/copy.svg" onClick={() => this.copy()} />}
+			</Host>
+		);
+	}
 }
