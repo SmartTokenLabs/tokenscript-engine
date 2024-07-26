569a90a0d61bb746f67d126df12e41384f598664
Michael Wallace <micwallace@gmx.com>
2024-07-26 14:15:55 +1000

    feat: Add JoyID EIP-1193 provider

---
+++ javascript/tokenscript-viewer/src/components/wallet/providers/JoyIDProvider.ts
@@ -0,0 +1,40 @@
+import { EthereumProvider, EvmConfig } from "@joyid/ethereum-provider";
+import {WC_V2_DEFAULT_CHAINS} from "./WalletConnectV2Provider";
+import {Chain} from "viem";
+import {CHAIN_CONFIG} from "../../../integration/constants";
+
+export const getJoyIDProviderInstance = async () => {
+
+	const chainConfig: Chain[] = WC_V2_DEFAULT_CHAINS.map((chainId) => {
+
+		const chainData = CHAIN_CONFIG[chainId];
+
+		return <Chain>{
+			id: chainId,
+			name: "ChainID " + chainId,
+			nativeCurrency: {
+				name: "Ethereum",
+				symbol: "ETH",
+				decimals: 18
+			},
+			rpcUrls: {
+				default: {
+					http: typeof chainData.rpc === "string" ? [chainData.rpc] : chainData.rpc
+				}
+			},
+			blockExplorers: chainData.explorer ? {
+				default: {
+					name: "Default explorer",
+					url: chainData.explorer,
+					apiUrl: ""
+				}
+			} : null,
+		}
+	});
+
+	return new EthereumProvider(chainConfig, {
+		name: "Smart Token Viewer",
+		logo: `${location.origin}/assets/icon/icon.png`,
+		joyidAppURL: "https://app.joy.id",
+	});
+};
