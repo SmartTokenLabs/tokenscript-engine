c026769a00c9e65e4151037ad35bcf21600d63cb
Justin Feng <realfengjia@foxmail.com>
2024-09-11 18:21:41 +0800

    chore: 🎸 export more modules for external engine usage - common-api

--- javascript/engine-js/src/index.ts
+++ javascript/engine-js/src/index.ts
@@ -1,4 +1,23 @@
 export { ITokenScriptEngine } from './IEngine';
 export { TokenScriptEngine } from './Engine';
+export { LiteTokenScriptEngine } from './LiteEngine';
 export { ITokenScript } from './ITokenScript';
 export { TokenScript } from './TokenScript';
+export { LiteTokenScript } from './LiteTokenScript';
+
+export { Meta } from './tokenScript/Meta';
+export { Label } from './tokenScript/Label';
+export { Card } from './tokenScript/Card';
+export { Cards } from './tokenScript/Cards';
+export { Origin } from './tokenScript/Origin';
+export { Contract } from './tokenScript/Contract';
+export { Contracts } from './tokenScript/Contracts';
+export { Transaction } from './tokenScript/Transaction';
+export { Attribute } from './tokenScript/Attribute';
+export { Attributes } from './tokenScript/Attributes';
+
+export { ScriptSourceType } from './IEngine';
+
+export { EthersAdapter, IChainConfig } from './wallet/EthersAdapter';
+
+export { TrustedKey } from './security/TrustedKeyResolver';
