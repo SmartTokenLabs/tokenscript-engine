27b7c1594fe72812678d1ea0e70226a718a949e6
futantan <luckytantanfu@gmail.com>
2024-11-13 18:55:16 +1100

    feat: 🎸 update share to x link

--- javascript/tokenscript-viewer/src/components/common/share-to-x-button.tsx
+++ javascript/tokenscript-viewer/src/components/common/share-to-x-button.tsx
@@ -1,10 +1,29 @@
 import { Component, h, Prop } from '@stencil/core';
-import { getTgUrl } from '../viewers/util/tgUrl';
 
 const X_POST_URL = "https://x.com/intent/tweet";
 
+function buildTlinkToShareOnTwitter() {
+	const url = new URL(document.location.href);
+	// change host to tlink.store/tapp
+	const newUrl = new URL('tapp', 'https://tlink.store');
+
+	url.searchParams.forEach((value, key) => {
+		newUrl.searchParams.set(key, value);
+	});
+
+	// add tokenId to the new url
+	const tokenIdMatch = url.hash.match(/tokenId=([^&]+)/);
+	if (tokenIdMatch) {
+		newUrl.searchParams.set('tokenId', tokenIdMatch[1]);
+	}
+
+	newUrl.hash = url.hash;
+
+	return newUrl.toString();
+}
+
 function getXPostUrl(){
-	return `${X_POST_URL}?text=${encodeURIComponent(document.location.href)+ " #SmartLayer #Tapps"}&related=SmartLayer`;
+	return `${X_POST_URL}?text=${encodeURIComponent(buildTlinkToShareOnTwitter()) + ' #SmartLayer #Tapps'}&related=SmartLayer`;
 }
 
 @Component({
@@ -25,7 +44,7 @@ export class XShareButton {
           title="Share on Telegram"
         >
           <img alt="Share to X.com" title="Share to X.com"
-               src="/assets/icon/x-icon.png" style={{ width: '100%', height: '100%' }} />
+               src="/assets/icon/x-icon.png" style={{ width: '100%', height: '100%', borderRadius: '9999px' }} />
         </a>
       </div>
     );
